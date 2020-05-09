6bf69cd57870e9ef13cf019ee6169a693239b3bd
Daroga1107 <daroga.1107@gmail.com>
2020-05-08 21:41:53 -0500

    segunda prueba

--- src/java/Actions/Login.java
+++ src/java/Actions/Login.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -56,7 +56,7 @@ public class Login extends ActionSupport{
                     return PROFESOR;
                 case ESTUDIANTE:
                     return ESTUDIANTE;                    
-            }
+            }  
         }
         addActionError("Usuario o contraseña incorrectos");
         return INPUT;           
